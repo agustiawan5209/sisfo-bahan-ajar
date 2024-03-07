c308755599d47a0259d97ae0411b20c884c535f3
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-03-07 15:29:55 +0800

    Buat Model, Controller, dan Migration Dari Kelas dan Topik

---
+++ app/Http/Controllers/TopikController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topik;
+use App\Http\Requests\StoreTopikRequest;
+use App\Http\Requests\UpdateTopikRequest;
+
+class TopikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTopikRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Topik $topik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Topik $topik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTopikRequest $request, Topik $topik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Topik $topik)
+    {
+        //
+    }
+}
